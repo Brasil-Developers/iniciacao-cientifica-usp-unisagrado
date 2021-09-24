977e255046b621a50950ea382589ef592d9d8ce5
Gabriel Furtado <gabrielft33@gmail.com>
2021-09-23 21:54:20 -0300

    Some more itens in register

--- src/Screens/registration/registration.tsx
+++ src/Screens/registration/registration.tsx
@@ -1,91 +1,189 @@
 // ICONS
-import { Alert, Input } from 'antd';
+// eslint-disable-next-line object-curly-newline
+import { Alert, Input, Radio, Select, Space } from 'antd';
+import React, { useState } from 'react';
 import FooterInitial from 'Screens/footer/footer-initial.tsx';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { ReactComponent as BackIcon } from '../../assets/icons/components/reset-password/back.svg';
 
 // STYLE COMPONENT
 import './registration-style.scss';
 
-const Resgistration = () => (
-  <>
-    <div className="login_page">
-      <div className="login_page__top_bar"></div>
-      <div className="login_page__content">
-        <div className="registration__content">
-          <div className="registration__btn_back">
-            <div className="btn_back">
-              <BackIcon className="icon_back" />
-              Voltar para a tela de login
+export default function Resgistration() {
+  const [valueOption, setValue] = useState(1);
+  const { Option } = Select;
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="login_page">
+        <div className="login_page__top_bar"></div>
+        <div className="login_page__content">
+          <div className="registration__content">
+            <div className="registration__btn_back">
+              <div className="btn_back">
+                <BackIcon className="icon_back" />
+                Voltar para a tela de login
+              </div>
             </div>
-          </div>
-          <div className="title">Solicitar Cadastro</div>
-          <div className="content_text">
-            Se você já foi cadastrado como avaliador neste portal mas não lembra
-            sua senha, selecione a função
-            <b> RECUPERAR SENHA </b>
-            na tela inicial. Se ainda não foi cadastrado,
-            <b> preencha o formulário abaixo </b>
-            para que um administrador do portal (Avaliador Master) possa
-            realizar seu cadastro. Após o preenchimento, fique atento à sua
-            caixa de e-mail. :)
-          </div>
-          <div className="box_important">
-            <Alert
-              message="Informational Notes"
-              description="Este portal é restrito a profissionais, pesquisadores e alunos
+            <div className="title">Solicitar Cadastro</div>
+            <div className="content_text">
+              Se você já foi cadastrado como avaliador neste portal mas não
+              lembra sua senha, selecione a função
+              <b> RECUPERAR SENHA </b>
+              na tela inicial. Se ainda não foi cadastrado,
+              <b> preencha o formulário abaixo </b>
+              para que um administrador do portal (Avaliador Master) possa
+              realizar seu cadastro. Após o preenchimento, fique atento à sua
+              caixa de e-mail. :)
+            </div>
+            <div className="box_important">
+              <Alert
+                message="Informational Notes"
+                description="Este portal é restrito a profissionais, pesquisadores e alunos
                           integrantes de equipes específicas, autorizadas pelo HRAC-USP.
                           Caso você não esteja enquadrado nessas características, não será
                           possível realizar seu cadastro. Em caso de dúvidas, entre em
                           contato através do e-mail exemplo@email.com.br."
-              type="info"
-              showIcon
-              closable
-            />
-          </div>
-          <div className="data-person_title">
-            <text className="title_blue">Dados pessoais</text>
-            <text className="subtitle">
-              {' '}
-              (Necessários para seu cadastro e identificação dentro do portal)
-            </text>
-            <div className="line"></div>
-            <div>
-              <table>
-                <td>
-                  <p className="input_title">
-                    Nome
-                    <i className="input_must">*</i>
-                  </p>
-                  <Input
-                    placeholder="Digite seu nome completo"
-                    style={{ width: '254px', marginRight: '16px' }}
-                  />
-                </td>
-                <td>
-                  <p className="input_title">
-                    Data de Nasc*
-                    <i className="input_must">*</i>
-                  </p>
-                  <Input
-                    placeholder="Digite seu nome completo"
-                    style={{ width: '150px', marginRight: '16px' }}
-                  />
-                </td>
-                <td>
-                  <p className="input_title">
-                    CPF
-                    <i className="input_must">*</i>
-                  </p>
-                  <Input placeholder="Digite seu nome completo" />
-                </td>
-              </table>
+                type="info"
+                showIcon
+                closable
+              />
+            </div>
+            <div className="data-person_title">
+              <text className="title_blue">Dados pessoais</text>
+              <text className="subtitle">
+                {' '}
+                (Necessários para seu cadastro e identificação dentro do portal)
+              </text>
+              <div className="line"></div>
+              <div className="table_forms">
+                <table>
+                  <td>
+                    <p className="input_title">
+                      Nome
+                      <i className="input_must">*</i>
+                    </p>
+                    <Input
+                      placeholder="Digite seu nome completo"
+                      style={{ width: '254px', marginRight: '16px' }}
+                    />
+                  </td>
+                  <td>
+                    <p className="input_title">
+                      Data de Nasc*
+                      <i className="input_must">*</i>
+                    </p>
+                    <Input
+                      placeholder="Digite seu nome completo"
+                      style={{ width: '150px', marginRight: '16px' }}
+                    />
+                  </td>
+                  <td>
+                    <p className="input_title">
+                      CPF
+                      <i className="input_must">*</i>
+                    </p>
+                    <Input placeholder="Digite seu nome completo" />
+                  </td>
+                </table>
+              </div>
+              <div className="table_forms">
+                <Radio.Group onChange={onChange} value={valueOption}>
+                  <Radio value={1}>Feminino</Radio>
+                  <Radio value={2}>Masculino</Radio>
+                  <Radio value={3}>Outro</Radio>
+                </Radio.Group>
+              </div>
+              <div className="table_forms">
+                <table>
+                  <td>
+                    <p className="input_title">
+                      Área de atuação
+                      <i className="input_must">*</i>
+                    </p>
+                    <Select style={{ width: 200 }} placeholder="Selecionar">
+                      <Option value="1">Astrologia</Option>
+                      <Option value="2">Vampirismo</Option>
+                      <Option value="3">Pompoarismo</Option>
+                      <Option value="4">Professor de Geografia</Option>
+                      <Option value="5">Atendente de sex shop</Option>
+                      <Option value="6">Vendedor da Avon</Option>
+                    </Select>
+                  </td>
+                  <td>
+                    <p className="input_title">
+                      Número do CRFa
+                      <i className="input_must">*</i>
+                    </p>
+                    <Input
+                      placeholder="_______________"
+                      style={{ width: '254px', marginRight: '16px' }}
+                    />
+                  </td>
+                </table>
+              </div>
+            </div>
+            <div className="data-person_title">
+              <text className="title_blue">E-mail e Senha</text>
+              <text className="subtitle">
+                {' '}
+                (Você precisará se lembrar deles para acessar o portal)
+              </text>
+              <div className="line"></div>
+              <div className="table_forms">
+                <table>
+                  <td>
+                    <p className="input_title">
+                      E-mail
+                      <i className="input_must">*</i>
+                    </p>
+                    <Input
+                      placeholder="Digite seu e-mail"
+                      style={{ width: '254px', marginRight: '16px' }}
+                    />
+                  </td>
+                </table>
+              </div>
+              <div className="table_forms">
+                <table>
+                  <td>
+                    <p className="input_title">
+                      Senha
+                      <i className="input_must">*</i>
+                    </p>
+                    <Space direction="vertical">
+                      <Input.Password
+                        placeholder="input password"
+                        // eslint-disable-next-line max-len
+                        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                      />
+                    </Space>
+                  </td>
+                  <td>
+                    <p className="input_title">
+                      Senha Confirmação
+                      <i className="input_must">*</i>
+                    </p>
+                    <Space direction="vertical">
+                      <Input.Password
+                        placeholder="input password"
+                        // eslint-disable-next-line max-len
+                        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                      />
+                    </Space>
+                  </td>
+                </table>
+              </div>
             </div>
           </div>
         </div>
+        <FooterInitial />
       </div>
-      <FooterInitial />
-    </div>
-  </>
-);
-
-export default Resgistration;
+    </>
+  );
+}
