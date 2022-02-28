f4073fddd094ad005455f5942dcdf13b2597da47
samuel <samueldasilvadev@gmail.com>
2022-02-27 21:44:56 -0300

    feat: [00034]: add screen redefine password

---
+++ src/routes/redefine-password.js
@@ -0,0 +1,8 @@
+import RedefinePassword from 'Screens/redefine-password';
+
+const redefinePassword = {
+  path: '/Redefinir_Senha',
+  component: RedefinePassword
+};
+
+export default redefinePassword;
