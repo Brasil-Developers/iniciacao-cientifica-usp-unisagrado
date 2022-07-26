38be8f58809a0ff2f3c2d5f7c864f5772bc7b526
Samuel da Silva <samueldasilvadev@gmail.com>
2022-07-25 22:31:45 -0300

    [00034] add records table

--- src/Screens/records/index.tsx
+++ src/Screens/records/index.tsx
@@ -1,6 +1,37 @@
-export default function Records() {
-  return (
-    <>
-    </>
-  );
-}
+import { Table } from 'antd';
+import { ModelCol4 } from 'Components/DefaultTable/Columns';
+
+// @note apenas para teste
+const tableColumns = [
+  {
+    key: 1,
+    tab: 'Cadastrados',
+    col: ModelCol4,
+    data: [
+      {
+        key: 1,
+      },
+    ],
+  },
+];
+
+const Records = () => (
+  <div>
+    {tableColumns.map((item: any) => (
+      <Table
+        columns={item.col}
+        dataSource={item.data}
+        rowSelection={{
+          type: 'checkbox',
+        }}
+        pagination={{
+          position: ['topRight'],
+          defaultCurrent: 1,
+          pageSize: 20,
+        }}
+      />
+    ))}
+  </div>
+);
+
+export default Records;
